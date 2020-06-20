362ce69a0e1779d184414f3ce3d0f34e305b9333
Yevhwnii <mirandoos3223@gmail.com>
2020-06-20 19:01:08 +0200

    # Switched to EJS templating engine

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// registers new templating engine in case it is not registered by express automatically
+// app.engine('hbs', expressHbs());
 // set global values on our app
-app.set('view engine', 'pug'); // says express to use pug/whatever as a templating engine (should have built-in support for express)
+app.set('view engine', 'ejs'); // says express to use pug/ejs as a templating engine (should have built-in support for express)
 app.set('views', 'views'); // says express where he can get templates
 
 const adminData = require('./routes/admin');
