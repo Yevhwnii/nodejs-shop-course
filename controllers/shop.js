376edf25f486ee6a2111317be7bde6c5221f687f
Yevhwnii <mirandoos3223@gmail.com>
2020-06-21 01:39:14 +0200

    # Adjusting controllers to new views

---
+++ controllers/shop.js
@@ -0,0 +1,34 @@
+const Product = require('../models/product');
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAllProducts((products) => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+    });
+  });
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAllProducts((products) => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All products',
+      path: '/products',
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'My Cart',
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    pageTitle: 'Checkout',
+  });
+};
