a35dd3124f21870670b6d843ed19cab978b1fbb8
Yevhwnii <mirandoos3223@gmail.com>
2020-06-20 21:41:24 +0200

    # Refactored product model

--- models/product.js
+++ models/product.js
@@ -1,18 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(__dirname, '../', 'data', 'products.json');
+// Explanation 1
+const getProductsFromFile = (callback) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return callback([]);
+    }
+    callback(JSON.parse(fileContent));
+  });
+};
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    const p = path.join(__dirname, '../', 'data', 'products.json');
-    fs.readFile(p, (error, fileContent) => {
-      let products = [];
-      if (!error) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
@@ -21,14 +27,7 @@ module.exports = class Product {
   }
 
   static fetchAllProducts(callback) {
-    // Explanation 1
-    const p = path.join(__dirname, '../', 'data', 'products.json');
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        return callback([]);
-      }
-      return callback(JSON.parse(fileContent));
-    });
+    getProductsFromFile(callback);
   }
 };
 
