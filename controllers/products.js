1e75cacc05860272018a401fccd79309a2e1bfd7
Yevhwnii <mirandoos3223@gmail.com>
2020-06-20 21:09:41 +0200

    # Adjustments due to product model

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', {
@@ -8,12 +8,16 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' });
+  const products = Product.fetchAllProducts();
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+  });
 };
-
-exports.products = products;
