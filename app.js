ef6f2f5bb2e8f5406132d6f0cac90e744b2d0218
Yevhwnii <mirandoos3223@gmail.com>
2020-06-20 19:54:30 +0200

    # Cleaning project from my comments

--- app.js
+++ app.js
@@ -5,11 +5,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// registers new templating engine in case it is not registered by express automatically
-// app.engine('hbs', expressHbs());
-// set global values on our app
-app.set('view engine', 'ejs'); // says express to use pug/ejs as a templating engine (should have built-in support for express)
-app.set('views', 'views'); // says express where he can get templates
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
