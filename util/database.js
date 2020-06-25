ac0761372d74f6df71c12c406020dff2598647f6
Yevhwnii <mirandoos3223@gmail.com>
2020-06-26 01:07:03 +0200

    # Adding MySQL

---
+++ util/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+// Pool will manage for us creating and closing connection on even multiple queries
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'nodejs_course',
+  password: 'ichigo32',
+}); // creating a pool so we don`t have to open and close connection for each query
+
+module.exports = pool.promise(); // allows us to use promises (async code)
