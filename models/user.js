40611004269825ab5ec37ecd11c25d68035e9630
Yevhwnii <mirandoos3223@gmail.com>
2020-06-26 15:52:26 +0200

    # User model

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize/types');
+
+// User should: be able to create a unique cart, hold there porducts, which means he has to relate to other tables
+const Sequelize = require('sequelize');
+
+const db = require('../util/database');
+
+const User = db.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: Sequelize.STRING,
+  email: Sequelize.STRING,
+});
+
+module.exports = User;
