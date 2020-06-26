332175c95680c43f8f5f89537908408ad523752d
Yevhwnii <mirandoos3223@gmail.com>
2020-06-26 02:26:38 +0200

    # Product model reworked

--- models/product.js
+++ models/product.js
@@ -1,32 +1,31 @@
+const Sequelize = require('sequelize');
+
 const db = require('../util/database');
 
-const Cart = require('./cart');
+// This a way you define models using Sequelize, all types are in docs
+const Product = db.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
-
-  save() {
-    return db.execute(
-      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-    // using ? mark we add security layer so that user cannot insert sql command in our database
-    // mysql checks syntax of this query, inserts our data and removes sql command
-  }
-
-  static fetchAllProducts() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-
-  static deleteById(id) {}
-};
+module.exports = Product;
