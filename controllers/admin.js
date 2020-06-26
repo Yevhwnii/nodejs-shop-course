df194cde2389c7e582a7edba172738b178185628
Yevhwnii <mirandoos3223@gmail.com>
2020-06-26 02:52:51 +0200

    # Admin porducts

--- controllers/admin.js
+++ controllers/admin.js
@@ -62,13 +62,15 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAllProducts((products) => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Product Managment',
-      path: '/admin/products',
-    });
-  });
+  Product.findAll()
+    .then((products) => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Product Managment',
+        path: '/admin/products',
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
