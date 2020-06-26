d607c357be3480375c1dc0d1e50a08a7a2630f99
Yevhwnii <mirandoos3223@gmail.com>
2020-06-26 02:19:54 +0200

    # Integrating Sequelize

--- util/database.js
+++ util/database.js
@@ -1,11 +1,22 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-// Pool will manage for us creating and closing connection on even multiple queries
-const pool = mysql.createPool({
+// // Pool will manage for us creating and closing connection on even multiple queries
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'nodejs_course',
+//   password: 'ichigo32',
+// }); // creating a pool so we don`t have to open and close connection for each query
+
+// module.exports = pool.promise(); // allows us to use promises (async code)
+
+////////////////////////////////////////////////////
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('nodejs_course', 'root', 'ichigo32', {
+  dialect: 'mysql',
   host: 'localhost',
-  user: 'root',
-  database: 'nodejs_course',
-  password: 'ichigo32',
-}); // creating a pool so we don`t have to open and close connection for each query
+});
 
-module.exports = pool.promise(); // allows us to use promises (async code)
+module.exports = sequelize;
