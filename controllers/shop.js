247fe15745ea71b3486bb3ebde3df95d3f725b06
Yevhwnii <mirandoos3223@gmail.com>
2020-07-06 13:33:50 +0300

    # Controllers for Order routes

--- controllers/shop.js
+++ controllers/shop.js
@@ -55,11 +55,12 @@ exports.getCart = (req, res, next) => {
       return cart
         .getProducts() // return products of the cart since it is associated with products (through argument)
         .then((products) => {
-          res.render('/shop/cart', {
+          res.render('shop/cart', {
             path: '/cart',
             pageTitle: 'My cart',
             products,
           });
+          // console.log(products[0].cartItems.quantity, 'ALO SUKA');
         })
         .catch((err) => console.log(err));
     })
@@ -68,16 +69,61 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
+  let fetchedCart;
 
-  Product.findById(prodId, (product) => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then((products) => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+      let newQuanity = 1;
+      if (product) {
+        const oldQuanity = product.cartItems.quantity;
+        newQuanity = oldQuanity + 1;
+        return fetchedCart.addProduct(product, {
+          through: { quantity: newQuanity },
+        });
+      }
+
+      return Product.findByPk(prodId)
+        .then((product) => {
+          return fetchedCart.addProduct(product, {
+            through: { quantity: newQuanity },
+          });
+        })
+        .catch((err) => console.log(err));
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.deleteCartItem = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then((products) => {
+      const product = products[0];
+      console.log(products, 'PRODUCT');
+
+      return product.cartItems.destroy();
+    })
+    .then((result) => {
+      res.redirect('/cart');
+    })
+    .catch((err) => console.log(err));
+
+  Product.findByPk(prodId).then((product) => {
     Cart.deleteProduct(prodId, product.price);
     res.redirect('/cart');
   });
@@ -94,4 +140,27 @@ exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     pageTitle: 'Checkout',
   });
+};
+
+exports.postOrder = (req, res, next) => {
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts();
+    })
+    .then((products) => {
+      return req.user.createOrder().then((order) => {
+        return order.addProducts(
+          products.map((product) => {
+            product.orderItem = { quantity: product.cartItems.quantity };
+            return product;
+          })
+        );
+        // Since we cant just easily add products to table, we modify each product so it can access quantity later
+      });
+    })
+    .then((result) => {
+      res.redirect('/orders');
+    })
+    .catch((err) => console.log(err));
 };
