720bc61a242e2f2d10ee0712e9023f6b23f85421
Yevhwnii <mirandoos3223@gmail.com>
2020-07-07 16:18:38 +0300

    # getDb method

--- util/database.js
+++ util/database.js
@@ -1,15 +1,33 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+// underscore means it is used only internally here
+let _db;
+
+// we create connection once, and then by getDb method we can interact with db
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    'mongodb+srv://breiter:qweqwe123123@nodejscourse.o73ks.mongodb.net/<dbname>?retryWrites=true&w=majority'
+    'mongodb+srv://breiter:qweqwe123123@nodejscourse.o73ks.mongodb.net/shop?retryWrites=true&w=majority'
   )
     .then((client) => {
       console.log('MongoDB is connected!');
-      callback(client);
+      // can specify database name as an argument
+      _db = client.db();
+      callback();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
