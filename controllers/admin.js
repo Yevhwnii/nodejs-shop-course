0e88d2eceb79fcda0baf7540c310007ab5490695
Yevhwnii <mirandoos3223@gmail.com>
2020-07-07 17:58:34 +0300

    # (NoSQL) Enabling admin functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -22,48 +22,48 @@ exports.postAddProduct = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-// exports.getEditProduct = (req, res, next) => {
-//   // accesing query params (...?edit=true&smth=smth for example)
-//   const editMode = req.query.edit;
-//   if (!editMode) {
-//     return res.redirect('/');
-//   }
-//   const prodId = req.params.productId;
-//   req.user.getProducts({ where: { id: prodId } }).then((products) => {
-//     const product = products[0];
-//     if (!product) {
-//       return res.redirect('/');
-//     }
-//     res.render('admin/edit-product', {
-//       pageTitle: 'Edit product',
-//       path: '/admin/edit-product',
-//       editing: editMode,
-//       product: product,
-//     });
-//   });
-// };
+exports.getEditProduct = (req, res, next) => {
+  // accesing query params (...?edit=true&smth=smth for example)
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId).then((product) => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product,
+    });
+  });
+};
 
-// exports.postEditProduct = (req, res, next) => {
-//   const prodId = req.body.id;
-//   const updatedTitle = req.body.title;
-//   const updatedPrice = req.body.price;
-//   const updatedImageUrl = req.body.imageUrl;
-//   const updatedDesc = req.body.description;
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.id;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
 
-//   Product.findByPk(prodId)
-//     .then((product) => {
-//       product.title = updatedTitle;
-//       product.price = updatedPrice;
-//       product.imageUrl = updatedImageUrl;
-//       product.description = updatedDesc;
-//       return product.save(); // method provided by sequelize, we return it because it returns promise so we can chain another then outside of it
-//     })
-//     .then((result) => {
-//       console.log('Product updated');
-//       res.redirect('/admin/products');
-//     })
-//     .catch((err) => console.log(err));
-// };
+  const product = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedDesc,
+    updatedImageUrl,
+    prodId
+  );
+  product
+    .save()
+    .then((result) => {
+      console.log('Product updated');
+      res.redirect('/admin/products');
+    })
+    .catch((err) => console.log(err));
+};
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll()
@@ -77,14 +77,11 @@ exports.getProducts = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-// exports.postDeleteProduct = (req, res, next) => {
-//   const prodId = req.body.productId;
-//   Product.findByPk(prodId)
-//     .then((product) => {
-//       return product.destroy();
-//     })
-//     .then(() => {
-//       res.redirect('/admin/products');
-//     })
-//     .catch((err) => console.log(err));
-// };
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId)
+    .then(() => {
+      res.redirect('/admin/products');
+    })
+    .catch((err) => console.log(err));
+};
