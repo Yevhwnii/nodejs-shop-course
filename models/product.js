bb77c1ffda816c18f3267dc80bb3055a59bf8f36
Yevhwnii <mirandoos3223@gmail.com>
2020-07-07 17:58:49 +0300

    # (NoSQL) Delete by id

--- models/product.js
+++ models/product.js
@@ -3,24 +3,31 @@ const getDb = require('../util/database').getDb;
 
 // Way of creating models through vanila MongoDb connection
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id ? mongodb.ObjectId(id) : null;
   }
 
   save() {
     const db = getDb();
+    let dbOperation;
+    if (this._id) {
+      // Update product
+      dbOperation = db.collection('products').updateOne(
+        {
+          _id: this._id,
+        },
+        { $set: this }
+      );
+    } else {
+      dbOperation = db.collection('products').insertOne(this); // we return here to make it possible to chain then block
+      // that will make this method return a promise.
+    }
     // Telling which collection to use
-    return (
-      db
-        .collection('products') // we return here to make it possible to chain then block
-        // that will make this method return a promise.
-        .insertOne(this)
-        .then((result) => {})
-        .catch((err) => console.log(err))
-    );
+    return dbOperation.then((result) => {}).catch((err) => console.log(err));
   }
 
   static fetchAll() {
@@ -49,6 +56,15 @@ class Product {
       .then((product) => {
         return product;
       })
+      .catch((err) => console.log(err));
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => {})
       .catch((err) => console.log(err));
   }
 }
