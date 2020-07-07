91d1ef17bdea46c6d0276289c89e44a218b62a83
Yevhwnii <mirandoos3223@gmail.com>
2020-07-07 17:22:11 +0300

    # (NoSQL-2) fetchAll, findById

--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 // Way of creating models through vanila MongoDb connection
@@ -17,11 +18,38 @@ class Product {
         .collection('products') // we return here to make it possible to chain then block
         // that will make this method return a promise.
         .insertOne(this)
-        .then((result) => {
-          console.log(result);
-        })
+        .then((result) => {})
         .catch((err) => console.log(err))
     );
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    // returns cursor first. calling to array, returns all documents in array
+    // should only be used when amount of doucments is not that big, otherwise use pagination
+    return db
+      .collection('products')
+      .find()
+      .toArray()
+      .then((products) => {
+        return products;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static findById(prodId) {
+    // simple comparison of prodId to _id wont work because _id is special type stored in BSON format
+    // so we have to convert it first and only then compare
+    const db = getDb();
+    // next return next document found by criteria
+    return db
+      .collection('products')
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        return product;
+      })
+      .catch((err) => console.log(err));
   }
 }
 
