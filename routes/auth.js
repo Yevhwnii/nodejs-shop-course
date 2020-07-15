b6d2cba768a67da04d0bdb0b4efee4076d50d7ca
Yevhwnii <mirandoos3223@gmail.com>
2020-07-15 14:56:02 +0300

    # signup routes

--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,9 @@ const authController = require('../controllers/auth');
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
-router.get('/signup', authController.getSignUp);
 router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);
+router.get('/signup', authController.getSignUp);
+router.post('/signup', authController.postSignUp);
 
 module.exports = router;
