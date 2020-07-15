a013223997cfcafd04034fc59b21aef41338691e
Yevhwnii <mirandoos3223@gmail.com>
2020-07-15 15:48:46 +0300

    # Passing csrf token to all views, comments

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
+// 3-rd party imports
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
-
+const csrf = require('csurf');
+// Local imports
 const errorController = require('./controllers/error');
 const User = require('./models/user');
-
+// Routes
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+// Constants
 const MONGODB_URI =
   'mongodb+srv://breiter:qweqwe123123@nodejscourse.o73ks.mongodb.net/shop';
-
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions',
 });
-
+const csrfProtection = csrf();
+// View engine
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
-
+// Package middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
@@ -36,6 +39,14 @@ app.use(
   })
 );
 
+// For any non-GET request this package will look for the existance of csrf token
+// in request body.
+// We pass this token to the views, and from there, we add to all forms, input with name _csrf and value of this token
+// it will be checked behind the scenes and compared.
+// This middleware assigns csrf variable to request and pass it forward
+app.use(csrfProtection);
+
+// Extracting user from database, and assigning its data to every request
 app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
@@ -48,13 +59,21 @@ app.use((req, res, next) => {
     .catch((err) => console.log(err));
 });
 
+// Assigning local variables to all views
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+// Enabling routes
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
 app.use(errorController.get404);
 
-// Way of enabling mongoose
+// Enabling mongoose
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
