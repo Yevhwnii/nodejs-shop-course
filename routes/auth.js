d83962bc499a331eb27d55794abe9709109168cb
Yevhwnii <mirandoos3223@gmail.com>
2020-07-20 17:27:28 +0300

    # Validating signup page and login page

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,52 @@
 const express = require('express');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
-router.post('/login', authController.postLogin);
+router.post(
+  '/login',
+  [
+    body('email').isEmail().withMessage('Please enter a valid email address.'),
+    body('password', 'Password has to be valid.')
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  ],
+  authController.postLogin
+);
 router.post('/logout', authController.postLogout);
 router.get('/signup', authController.getSignUp);
-router.post('/signup', authController.postSignUp);
+router.post(
+  '/signup',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email')
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject('Email exists already, try different one');
+          }
+        });
+      }),
+    body(
+      'password',
+      'Please enter password contains numbers and text and at least 5 characters'
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body('confirmPassword').custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords have to match!');
+      }
+      return true;
+    }),
+  ],
+  authController.postSignUp
+);
 
 router.get('/reset', authController.getReset);
 router.post('/reset', authController.postReset);
