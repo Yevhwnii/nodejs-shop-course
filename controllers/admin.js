86533986e0296e98a47f9bb9a4878322fb5c0d6b
Yevhwnii <mirandoos3223@gmail.com>
2020-07-26 16:37:21 +0300

    # adjustment concerning delete product

--- controllers/admin.js
+++ controllers/admin.js
@@ -176,9 +176,9 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
+exports.deleteProduct = (req, res, next) => {
   // Delete product from db and product image from server
-  const prodId = req.body.productId;
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then((product) => {
       if (!product) {
@@ -188,11 +188,13 @@ exports.postDeleteProduct = (req, res, next) => {
       return Product.deleteOne({ _id: prodId, userId: req.user._id });
     })
     .then(() => {
-      res.redirect('/admin/products');
+      res.status(200).json({
+        message: 'Success!',
+      });
     })
     .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({
+        message: 'Deleting product failed',
+      });
     });
 };
