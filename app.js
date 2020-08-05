404b22c59a7c430d4c836e234d2b324a06bd3103
Yevhwnii <mirandoos3223@gmail.com>
2020-08-05 14:55:59 +0300

    # prepared for deployment

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ const store = new MongoDBStore({
   uri: process.env.MONGO_URI,
   collection: 'sessions',
 });
-const privateKey = fs.readFileSync('server.key');
-const certificate = fs.readFileSync('server.cert');
+// const privateKey = fs.readFileSync('server.key');
+// const certificate = fs.readFileSync('server.cert');
 const csrfProtection = csrf();
 // View engine
 app.set('view engine', 'ejs');
